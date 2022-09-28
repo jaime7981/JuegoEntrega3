107969755181db134a6fcbecb75311fca174c08e
jaime7981 <jmfernandez1@miuandes.cl>
2022-09-27 21:32:09 -0300

    dockerized app update

--- web_app/mobile_game/mobile_game/settings.py
+++ web_app/mobile_game/mobile_game/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xi(j72m)n^0vffh$p^f3kyive8c%l&v=s6w4=)dt3z6yp6zk$v'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -73,10 +74,23 @@ WSGI_APPLICATION = 'mobile_game.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}'''
+
+# Production D
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': f'{os.environ["DB_NAME"]}',
+        'USER': f'{os.environ["DB_USER"]}',
+        'PASSWORD': f'{os.environ["DB_PASSWD"]}',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
