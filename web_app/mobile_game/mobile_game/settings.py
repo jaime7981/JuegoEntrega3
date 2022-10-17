ccb0aedf0aa11fddd8528e00892e89a81134ba82
jaime7981 <jmfernandez1@miuandes.cl>
2022-10-16 21:31:07 -0300

    settings static root path

--- web_app/mobile_game/mobile_game/settings.py
+++ web_app/mobile_game/mobile_game/settings.py
@@ -165,6 +165,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_collect')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
